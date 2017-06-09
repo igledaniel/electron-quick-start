b5eb63cd4255f8271a10b93dbe7e33e8e6101bff
Diana Shkolnikov <diana@mapzen.com>
2017-06-09 18:26:30 -0400

    move api_key to variable and change initial view

--- index_renderer.js
+++ index_renderer.js
@@ -1,7 +1,24 @@
 const { ipcRenderer } = require('electron');
 const settings = require('electron-settings');
 
+const api_key = 'mapzen-fepXwQF';
+let map;
+
 // handle window loading event
 document.getElementById('body').onload = function () {
   console.log('good news: index window is loading!');
+
+  initMap();
 };
+
+function initMap() {
+  // Add a Mapzen API key
+  L.Mapzen.apiKey = api_key;
+  map = L.Mapzen.map('map', { maxZoom: 18, minZoom: 2 });
+  
+  // Set the center of the map to be the San Francisco Bay Area at zoom level 12
+  map.setView([40, -90], 4);
+
+  var geocoder = L.Mapzen.geocoder();
+  geocoder.addTo(map);
+}
