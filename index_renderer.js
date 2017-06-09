7a3c7845959db047efe747a0dc616b7edbf4d32f
Diana Shkolnikov <diana@mapzen.com>
2017-06-09 17:56:38 -0400

    Create index_renderer.js

--- index_renderer.js
+++ index_renderer.js
@@ -1,7 +1,5 @@
 const { ipcRenderer } = require('electron');
 const settings = require('electron-settings');
-const config = require('./config.json');
-
 
 // handle window loading event
 document.getElementById('body').onload = function () {
