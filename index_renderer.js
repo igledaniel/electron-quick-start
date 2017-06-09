2e83f653122634dc8e6d6bff7fddf39752ae634c
Diana Shkolnikov <dianashk@gmail.com>
2017-06-09 17:44:43 -0400

    add reverse geocoding to map

--- index_renderer.js
+++ index_renderer.js
@@ -2,7 +2,13 @@ const { ipcRenderer } = require('electron');
 const settings = require('electron-settings');
 const config = require('./config.json');
 
+const mapzenSearch = require('./mapzen-util/src/js/mapzenSearch');
+
 let map;
+let marker;
+let search;
+
+const api_key = 'mapzen-fepXwQF';
 
 // handle window loading event
 document.getElementById('body').onload = function () {
@@ -13,12 +19,52 @@ document.getElementById('body').onload = function () {
 
 function initMap() {
   // Add a Mapzen API key
-  L.Mapzen.apiKey = 'mapzen-fepXwQF';
+  L.Mapzen.apiKey = api_key;
   map = L.Mapzen.map('map', { maxZoom: 18, minZoom: 2 });
   
   // Set the center of the map to be the San Francisco Bay Area at zoom level 12
-  map.setView([0, 0], 2);
+  map.setView([40, -90], 4);
 
   var geocoder = L.Mapzen.geocoder();
   geocoder.addTo(map);
+
+  addClickHandler();
+}
+
+// Add click event handling to perform the reverse geocoding and draw the results on the map.
+function addClickHandler() {
+  map.on('click', function (e) {
+
+    // configuration for the reverse geocoding query
+    const reverseOptions = {
+      host: config.search.host,
+      api_key: api_key,
+      endpoint: 'reverse',
+      params: {
+        "point.lat": e.latlng.lat,
+        "point.lon": e.latlng.lng,
+        "layers": "region"
+      }
+    };
+
+    // query mapzen search asynchronously and handle results as desired
+    mapzenSearch(reverseOptions, (err, results) => {
+      dropMarker(results);
+    });
+  });
+}
+
+// Draw the POI marker (just geojson with points, no polygons)
+function dropMarker(place) {
+  // if previous marker existed, remove it
+  if (marker) {
+    map.removeLayer(marker);
+  }
+
+  marker = L.geoJson(place, {
+    onEachFeature: function (feature, layer) {
+      // show label when marker is clicked
+      layer.bindPopup(feature.properties.label);
+    }
+  }).addTo(map);
 }
---
+++ mapzen-util/src/js/mapzenSearch.js
@@ -0,0 +1,26 @@
+const querystring = require('querystring');
+const request = require('request');
+
+/**
+ * @param {object} options 
+ * @param {string} options.host 
+ * @param {string} options.api_key 
+ * @param {string} options.endpoint 
+ * @param {string} options.params 
+ * @param {function} callback 
+ */
+function query(options, callback) {
+  const { host, api_key, endpoint, params } = options;
+
+  const query = `${host}/v1/${endpoint}?api_key=${api_key}&${querystring.stringify(params)}`;
+  request.get(query, (err, res) => {
+    if (err) {
+      return callback(err);
+    }
+
+    var results = JSON.parse(res.body);
+    callback(null, results);
+  });
+}
+
+module.exports = query;
